eb5e0562757db444fd91d23c3acc6cd4cac223ad
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-11-09 16:27:40 +0100

    Drawn map prototype

--- src/main.c
+++ src/main.c
@@ -6,14 +6,15 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 14:06:57 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/11/09 15:31:08 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/11/09 16:27:37 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 #include "../libft/libft.h"
 
-void ft_drawn_line (char **map_data, t_map *map);
+void ft_drawn_line (char **map_data, t_map *map, int line);
+void ft_drawn_map (char **map_data, t_map *map, int line);
 
 void	hook(void *param)
 {
@@ -83,15 +84,18 @@ int	main(int argc, char *argv[])
 	ft_map_init(map);
 	map->mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
 	map->map_data = ft_get_map(argv);
-	ft_drawn_line(map->map_data, map);
+	ft_drawn_line(map->map_data, map, 1);
+	ft_drawn_line(map->map_data, map, 2);
+	ft_drawn_line(map->map_data, map, 3);
+	ft_drawn_line(map->map_data, map, 4);
+	ft_drawn_line(map->map_data, map, 5);
 	mlx_loop(map->mlx);
 	mlx_terminate(map->mlx);
 	return (0);
 }
 
-void ft_drawn_line (char **map_data, t_map *map)
+void ft_drawn_line (char **map_data, t_map *map, int line)
 {
-	int i;
 	int j;
 	char	**m_data;
 	t_map * tmp_map;
@@ -99,17 +103,22 @@ void ft_drawn_line (char **map_data, t_map *map)
 	mlx_image_t* blocks;
 	blocks = mlx_new_image(map->mlx, 50, 50);
 	m_data = map_data;
-	i = 1;
 	j = 0;
 	drawn_block(blocks);
 
-	while (m_data[i][j] != '\0')
+	while (m_data[line][j] != '\0')
 	{
-		ft_printf("%c", m_data[i][j]);
-		if (m_data[i][j] == '1')
+		ft_printf("%c", m_data[line][j]);
+		if (m_data[line][j] == '1')
 		{
-			mlx_image_to_window(map->mlx, blocks, (j * 50), 0);
+			mlx_image_to_window(map->mlx, blocks, (j * 50), ((line -1) * 50));
 		}
+		
 		j++;
 	}
+}
+
+void ft_drawn_map (char **map_data, t_map *map, int line)
+{
+
 }
