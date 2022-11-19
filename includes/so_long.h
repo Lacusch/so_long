4e7cf33c87413ba7f2797b14fc684338dee582bb
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-11-19 13:30:16 +0100

    norm fix

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heibronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 13:10:40 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/11/19 13:28:17 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/11/19 13:30:06 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <fcntl.h>
 # include "../MLX42/include/MLX42/MLX42.h"
-#include "../libft/libft.h"
+# include "../libft/libft.h"
 // Defines:
 
 # define IMAC_RES_Y 1344
